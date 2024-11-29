aa6341f3abb80474101c0cef3bbaeba153c8769e
Eduard Ciofu <cfedward7@gmail.com>
2024-11-29 02:44:51 +0100

    Enemy AI Behavior Tree Setup

---
+++ Source/Aura/Private/AI/AuraAIController.cpp
@@ -0,0 +1,14 @@
+// Copyright Eduard Ciofu
+
+
+#include "AI/AuraAIController.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+AAuraAIController::AAuraAIController()
+{
+	Blackboard = CreateDefaultSubobject<UBlackboardComponent>("BlackboardComponent");
+	check(Blackboard);
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>("BehaviorTreeComponent");
+	check(BehaviorTreeComponent);
+}
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -9,6 +9,8 @@
 #include "UI/WidgetController/OverlayWidgetController.h"
 #include "AuraEnemy.generated.h"
 
+class AAuraAIController;
+class UBehaviorTree;
 class UWidgetComponent;
 /**
  * 
@@ -21,6 +23,8 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 public:
 
 	AAuraEnemy();
+
+	virtual void PossessedBy(AController* NewController) override;
 	
 	/** Enemy Interface */
 	virtual void HighlightActor() override;
@@ -62,5 +66,10 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UWidgetComponent> HealthBar;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	TObjectPtr<UBehaviorTree> BehaviorTree;
+	UPROPERTY()
+	TObjectPtr<AAuraAIController> AuraAIController;
 	
 };
