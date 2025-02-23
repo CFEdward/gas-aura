b92f93fce54024bade28f7f2cd92d00e9559bec9
Eduard Ciofu <cfedward7@gmail.com>
2025-02-23 16:57:36 +0100

    Created Cooldown Async Task

---
+++ Source/Aura/Private/AbilitySystem/AsyncTasks/WaitCooldownChange.cpp
@@ -0,0 +1,63 @@
+// Copyright Eduard Ciofu
+
+
+#include "AbilitySystem/AsyncTasks/WaitCooldownChange.h"
+
+#include "AbilitySystemComponent.h"
+
+UWaitCooldownChange* UWaitCooldownChange::WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& InCooldownTag)
+{
+	if (!IsValid(AbilitySystemComponent) || !InCooldownTag.IsValid()) return nullptr;
+	
+	UWaitCooldownChange* WaitCooldownChange = NewObject<UWaitCooldownChange>();
+	WaitCooldownChange->ASC = AbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = InCooldownTag;
+
+	// To know when a cooldown has ended (CooldownTag has been removed)
+	AbilitySystemComponent->RegisterGameplayTagEvent(InCooldownTag, EGameplayTagEventType::NewOrRemoved).AddUObject(
+		WaitCooldownChange,
+		&UWaitCooldownChange::CooldownTagChanged
+	);
+	// To know when a cooldown effect has been applied
+	AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(WaitCooldownChange, &UWaitCooldownChange::OnActiveEffectAdded);
+
+	return WaitCooldownChange;
+}
+
+void UWaitCooldownChange::CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount)
+{
+	if (NewCount == 0)
+	{
+		CooldownEnd.Broadcast(0.f);
+	}
+}
+
+void UWaitCooldownChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetASC,
+	const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveGameplayEffect)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+	{
+		const FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(CooldownTag.GetSingleTagContainer());
+		const TArray<float> TimesRemaining = ASC->GetActiveEffectsTimeRemaining(GameplayEffectQuery);
+		// Just in case there are more than 1 CooldownTags assigned (shouldn't be)
+		const float TimeRemaining = FMath::Max(TimesRemaining);
+
+		CooldownStart.Broadcast(TimeRemaining);
+	}
+}
+
+void UWaitCooldownChange::EndTask()
+{
+	if (!IsValid(ASC)) return;
+	
+	ASC->RegisterGameplayTagEvent(CooldownTag, EGameplayTagEventType::NewOrRemoved).RemoveAll(this);
+	ASC->OnActiveGameplayEffectAddedDelegateToSelf.RemoveAll(this);
+
+	SetReadyToDestroy();
+	//MarkAsGarbage();	// Unnecessary?
+}
