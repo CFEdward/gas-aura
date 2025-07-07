a54e906fa47821f93e9a8e5345e3bfcc0dcc752e
Eduard Ciofu <cfedward7@gmail.com>
2025-07-07 16:34:47 +0200

    Magic Circle

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright Eduard Ciofu
+
+
+#include "Actor/MagicCircle.h"
+
+#include "Components/DecalComponent.h"
+
+
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecalComp = CreateDefaultSubobject<UDecalComponent>("Magic Circle Decal");
+	MagicCircleDecalComp->SetupAttachment(GetRootComponent());
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -0,0 +1,28 @@
+﻿// Copyright Eduard Ciofu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class AURA_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AMagicCircle();
+
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecalComp;
+};
