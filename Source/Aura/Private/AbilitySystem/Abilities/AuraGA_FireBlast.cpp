ee2cd7d100833edffa5279de02e426108715402b
Eduard Ciofu <cfedward7@gmail.com>
2025-07-13 14:29:48 +0200

    Aura Fire Ball

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGA_FireBlast.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright Eduard Ciofu
+
+
+#include "AbilitySystem/Abilities/AuraGA_FireBlast.h"
+
+UAuraGA_FireBlast::UAuraGA_FireBlast()
+{
+	MaxNumSubSpell = 12;
+}
+
+TArray<AAuraFireBall*> UAuraGA_FireBlast::SpawnFireBalls()
+{
+	return TArray<AAuraFireBall*>();
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraGA_FireBlast.h
@@ -0,0 +1,27 @@
+﻿// Copyright Eduard Ciofu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraDamageGameplayAbility.h"
+#include "AuraGA_FireBlast.generated.h"
+
+class AAuraFireBall;
+
+UCLASS()
+class AURA_API UAuraGA_FireBlast : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UAuraGA_FireBlast();
+
+	UFUNCTION(BlueprintCallable)
+	TArray<AAuraFireBall*> SpawnFireBalls();
+
+private:
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AAuraFireBall> FireBallClass;
+};
