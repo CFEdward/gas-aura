b1a29f4306878239f049f247a4df66160e41e902
Eduard Ciofu <cfedward7@gmail.com>
2025-07-13 15:11:02 +0200

    Spawning FireBalls

--- Source/Aura/Private/AbilitySystem/Abilities/AuraGA_FireBlast.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGA_FireBlast.cpp
@@ -3,6 +3,9 @@
 
 #include "AbilitySystem/Abilities/AuraGA_FireBlast.h"
 
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Actor/AuraFireBall.h"
+
 UAuraGA_FireBlast::UAuraGA_FireBlast()
 {
 	MaxNumSubSpell = 12;
@@ -10,5 +13,30 @@ UAuraGA_FireBlast::UAuraGA_FireBlast()
 
 TArray<AAuraFireBall*> UAuraGA_FireBlast::SpawnFireBalls()
 {
-	return TArray<AAuraFireBall*>();
+	checkf(FireBallClass, TEXT("No FireBall class selected!"));
+	
+	TArray<AAuraFireBall*> FireBalls;
+	
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	TArray<FRotator> Rotators = UAuraAbilitySystemLibrary::EvenlySpacedRotators(Forward, FVector::UpVector, 360.f, MaxNumSubSpell);
+	for (const FRotator& Rotator : Rotators)
+	{
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(Location);
+		SpawnTransform.SetRotation(Rotator.Quaternion());
+		AAuraFireBall* FireBall = GetWorld()->SpawnActorDeferred<AAuraFireBall>(
+			FireBallClass,
+			SpawnTransform,
+			GetOwningActorFromActorInfo(),
+			CurrentActorInfo->PlayerController->GetPawn(),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn
+		);
+		FireBall->DamageEffectParams = MakeDamageEffectParamsFromClassDefaults();
+
+		FireBalls.Add(FireBall);
+		FireBall->FinishSpawning(SpawnTransform);
+	}
+	
+	return FireBalls;
 }
